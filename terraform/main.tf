5c805be52add114c6778b7528445f433d681dc86
Alexander Kruglyak <alexkruglyak23@gmail.com>
2026-02-18 21:50:38 +0200

    feat: add FRONTEND_URL to backend ConfigMap and implement IAM policy for AWS Load Balancer Controller in Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -227,14 +227,107 @@ resource "aws_iam_role" "alb_controller" {
   tags               = var.tags
 }
 
-resource "aws_iam_role_policy_attachment" "alb_controller" {
-  role       = aws_iam_role.alb_controller.name
-  policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
+resource "aws_iam_policy" "alb_controller" {
+  name        = "${var.project_name}-alb-controller-policy"
+  description = "IAM policy for AWS Load Balancer Controller"
+  tags        = var.tags
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["iam:CreateServiceLinkedRole"]
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "iam:AWSServiceName" = "elasticloadbalancing.amazonaws.com"
+          }
+        }
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeAccountAttributes", "ec2:DescribeAddresses",
+          "ec2:DescribeAvailabilityZones", "ec2:DescribeInternetGateways",
+          "ec2:DescribeVpcs", "ec2:DescribeVpcPeeringConnections",
+          "ec2:DescribeSubnets", "ec2:DescribeSecurityGroups",
+          "ec2:DescribeInstances", "ec2:DescribeNetworkInterfaces",
+          "ec2:DescribeTags", "ec2:DescribeCoipPools",
+          "elasticloadbalancing:DescribeLoadBalancers",
+          "elasticloadbalancing:DescribeLoadBalancerAttributes",
+          "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:DescribeListenerCertificates",
+          "elasticloadbalancing:DescribeSSLPolicies",
+          "elasticloadbalancing:DescribeRules",
+          "elasticloadbalancing:DescribeTargetGroups",
+          "elasticloadbalancing:DescribeTargetGroupAttributes",
+          "elasticloadbalancing:DescribeTargetHealth",
+          "elasticloadbalancing:DescribeTags",
+          "elasticloadbalancing:DescribeListenerAttributes",
+          "cognito-idp:DescribeUserPoolClient",
+          "acm:ListCertificates", "acm:DescribeCertificate",
+          "iam:ListServerCertificates", "iam:GetServerCertificate",
+          "wafv2:GetWebACL", "wafv2:GetWebACLForResource",
+          "wafv2:AssociateWebACL", "wafv2:DisassociateWebACL",
+          "waf-regional:GetWebACLForResource", "waf-regional:GetWebACL",
+          "waf-regional:AssociateWebACL", "waf-regional:DisassociateWebACL",
+          "shield:GetSubscriptionState", "shield:DescribeProtection",
+          "shield:CreateProtection", "shield:DeleteProtection"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:AuthorizeSecurityGroupIngress", "ec2:RevokeSecurityGroupIngress",
+          "ec2:CreateSecurityGroup", "ec2:DeleteSecurityGroup",
+          "ec2:CreateTags", "ec2:DeleteTags"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticloadbalancing:CreateLoadBalancer",
+          "elasticloadbalancing:CreateTargetGroup"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticloadbalancing:CreateListener",
+          "elasticloadbalancing:DeleteListener",
+          "elasticloadbalancing:CreateRule",
+          "elasticloadbalancing:DeleteRule",
+          "elasticloadbalancing:AddTags",
+          "elasticloadbalancing:RemoveTags",
+          "elasticloadbalancing:ModifyLoadBalancerAttributes",
+          "elasticloadbalancing:SetIpAddressType",
+          "elasticloadbalancing:SetSecurityGroups",
+          "elasticloadbalancing:SetSubnets",
+          "elasticloadbalancing:DeleteLoadBalancer",
+          "elasticloadbalancing:ModifyTargetGroup",
+          "elasticloadbalancing:ModifyTargetGroupAttributes",
+          "elasticloadbalancing:DeleteTargetGroup",
+          "elasticloadbalancing:RegisterTargets",
+          "elasticloadbalancing:DeregisterTargets",
+          "elasticloadbalancing:SetWebAcl",
+          "elasticloadbalancing:ModifyListener",
+          "elasticloadbalancing:AddListenerCertificates",
+          "elasticloadbalancing:RemoveListenerCertificates",
+          "elasticloadbalancing:ModifyRule"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "alb_controller_ec2" {
+resource "aws_iam_role_policy_attachment" "alb_controller" {
   role       = aws_iam_role.alb_controller.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+  policy_arn = aws_iam_policy.alb_controller.arn
 }
 
 resource "helm_release" "alb_controller" {
